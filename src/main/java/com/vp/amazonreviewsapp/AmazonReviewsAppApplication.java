b3876928c862c8135828e13a6e737d5ddfa9378a
vladpetrovskyi <v.petrovskyi98@gmail.com>
2020-06-18 19:46:17 +0300

    initial commit

---
+++ src/main/java/com/vp/amazonreviewsapp/AmazonReviewsAppApplication.java
@@ -0,0 +1,13 @@
+package com.vp.amazonreviewsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonReviewsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonReviewsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vp/amazonreviewsapp/AmazonReviewsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vp.amazonreviewsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonReviewsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
