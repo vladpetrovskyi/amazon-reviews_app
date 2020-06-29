19f6fe9369ac58df861bf573eb0decbf52b44715
vladpetrovskyi <v.petrovskyi98@gmail.com>
2020-06-29 10:53:56 +0300

    added response DTOs

---
+++ src/main/java/com/vp/amazonreviewsapp/model/dto/ProductResponseDto.java
@@ -0,0 +1,14 @@
+package com.vp.amazonreviewsapp.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ProductResponseDto {
+    private String id;
+}
