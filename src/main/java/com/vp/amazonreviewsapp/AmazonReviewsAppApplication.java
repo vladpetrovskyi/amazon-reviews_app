af18dc6cabe5e847a46aed82bd08b1c669d1fe37
vladpetrovskyi <v.petrovskyi98@gmail.com>
2020-06-29 10:56:35 +0300

    added some beans

--- src/main/java/com/vp/amazonreviewsapp/AmazonReviewsAppApplication.java
+++ src/main/java/com/vp/amazonreviewsapp/AmazonReviewsAppApplication.java
@@ -1,13 +1,38 @@
 package com.vp.amazonreviewsapp;
 
+import java.util.concurrent.Executor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InjectionPoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
+@EnableAsync
 public class AmazonReviewsAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AmazonReviewsAppApplication.class, args);
     }
 
+    @Bean("taskExecutor")
+    public Executor taskExecutor() {
+        final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(8);
+        executor.setThreadNamePrefix("Thread-");
+        executor.initialize();
+        return executor;
+    }
+
+    @Bean
+    @Scope("prototype")
+    public Logger produceLogger(InjectionPoint injectionPoint) {
+        Class<?> classOnWired = injectionPoint.getMember().getDeclaringClass();
+        return LoggerFactory.getLogger(classOnWired);
+    }
 }
