30d47c4776e585236109ac0728beffb6ed648926
vladpetrovskyi <v.petrovskyi98@gmail.com>
2020-06-29 10:54:11 +0300

    updated repositories

--- src/main/java/com/vp/amazonreviewsapp/repository/ProductRepository.java
+++ src/main/java/com/vp/amazonreviewsapp/repository/ProductRepository.java
@@ -1,7 +1,6 @@
 package com.vp.amazonreviewsapp.repository;
 
 import com.vp.amazonreviewsapp.model.Product;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, String> {
